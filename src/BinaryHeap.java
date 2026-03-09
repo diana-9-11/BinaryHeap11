a3a533ad46c61cbdd3d4682e4646b3a56761497f
diana-9-11 <shukshanova.d@gjh.sk>
2026-03-09 09:09:31 +0100

    Innitial commit

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class BinaryHeap {
-    public static void main(String[] args) {
-        System.out.println("Binary Heap");
+    //Binary max heap implementation
+    int[] data;
+    int size; //current number of elements in the heap
+    public BinaryHeap() {
+        data = new int[10]; //initial capacity of 10, can be increased later
+        size = 0;
+    }
+
+    public void ensureCapacity(){
+        if(data.length == size){
+            data = Arrays.copyOf(data, data.length * 2);
+        }
     }
 }
